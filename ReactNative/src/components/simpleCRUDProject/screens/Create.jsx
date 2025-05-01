57078ad80c4fc9c5edf48cbe3ecb784f218480a8
Prabin Joshi <joshiprabin17@gmail.com>
2025-05-01 13:37:17 +0545

    Implement item creation and management in Create component; refactor HomeScreen to manage item data state

--- ReactNative/src/components/simpleCRUDProject/screens/Create.jsx
+++ ReactNative/src/components/simpleCRUDProject/screens/Create.jsx
@@ -1,13 +1,213 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { useState } from 'react';
+import { FlatList, Pressable, StyleSheet, Text, TextInput, View } from 'react-native'
 
-const Create = () => {
+const Create = ({ data, setData }) => {
+
+    const [itemName, setItemName] = useState(''); // State to manage the name of the item
+    const [stock, setStock] = useState(''); // State to manage the stock amount of the item
+    const [isEdit, setIsEdit] = useState(false); // State to manage if the form is in edit mode
+    const [editItemId, setEditItemId] = useState(null); // State to manage the ID of the item being edited
+    const [unit, setUnit] = useState('kg'); // State to manage the unit of the item (default is kg)
+
+    // Function to handle adding a new item
+    const handlerAddItem = () => {
+        // Check if itemName and stock are not empty
+        if (!itemName || !stock) {
+            alert("Please fill all fields");
+            return;
+        }
+
+        // Prevent adding an item while in edit mode
+        if (isEdit) {
+            alert("Please click on the edit button to update the item");
+            return;
+        }
+
+        // Check if itemName already exists in the list
+        if (data.some((item) => item.name === itemName)) {
+            alert("Item already exists in the list");
+            return;
+        }
+
+        // check if itemName must be a string not a number or integer
+        if (!isNaN(itemName)) {
+            alert("Item name must be a string not a number or integer");
+            return;
+        }
+
+        // Check if stock is a number
+        if (isNaN(stock)) {
+            alert("Stock must be a number");
+            return;
+        }
+
+        // Check if stock is less than 0
+        if (stock < 0) {
+            alert("Stock must be greater than 0");
+            return;
+        }
+
+        // Check if stock is less than 100
+        if (stock > 100) {
+            alert("Stock must be less than 100");
+            return;
+        }
+
+        // Check if stock is an integer
+        if (stock % 1 !== 0) {
+            alert("Stock must be an integer");
+            return;
+        }
+
+        // Create a new item object
+        const newItem = {
+            id: Date.now(),
+            name: itemName,
+            stock: stock,
+            unit: unit,
+        };
+        // Update the data state with the new item
+        setData([...data, newItem]);
+        setItemName('');
+        setStock('');
+        setUnit('');
+        setIsEdit(false);
+    };
+
+    // Function to handle deleting an item
+    const deleteItemHandler = (id) => {
+        const newData = data.filter((item) => item.id !== id);
+        setData(newData);
+    };
+
+    // Function to handle editing an item
+    const editItemHandler = (item) => {
+        setItemName(item.name);
+        setStock(item.stock.toString());
+        setUnit(item.unit);
+        setIsEdit(true);
+        setEditItemId(item.id);
+    };
+
+    // Function to handle updating an item
+    const updateItemHandler = () => {
+        const updatedData = data.map((item) => {
+            if (item.id === editItemId) {
+                return { ...item, name: itemName, stock: stock };
+            }
+            return item;
+        });
+        setData(updatedData);
+        setItemName('');
+        setStock('');
+        setUnit('');
+        setIsEdit(false);
+    };
+
     return (
-        <View>
-            <Text>Create</Text>
+        <View style={styles.container}>
+            {/* Input field for item name */}
+            <TextInput
+                placeholder='Enter an item name...'
+                placeholderTextColor={'#A9A9A9'}
+                style={styles.input}
+                value={itemName}
+                onChangeText={(item) => setItemName(item)}
+            ></TextInput>
+
+            {/* Input field for stock amount */}
+            <TextInput
+                placeholder='Enter stock amount...'
+                placeholderTextColor={'#A9A9A9'}
+                style={styles.input}
+                value={stock}
+                onChangeText={(item) => setStock(item)}
+            ></TextInput>
+
+            {/* Button to add or update an item */}
+            <Pressable style={styles.btn} onPress={() => isEdit ? updateItemHandler() : handlerAddItem()}>
+                <Text style={{ color: "white", fontWeight: "bold", fontSize: 15 }}>{isEdit ? "Edit Item in Stock" : "Add Item in Stock"}</Text>
+            </Pressable>
+
+            <View style={{ marginTop: 10 }}>
+                {/* Heading for the list of items */}
+                <Text style={styles.headingText}>All Items in Stock</Text>
+
+                {/* List of items */}
+                <FlatList
+                    data={data}
+                    keyExtractor={(item) => item.id}
+                    renderItem={({ item }) => (
+                        <View style={[styles.itemContainer, { backgroundColor: item.stock < 20 ? '#f8d7da' : '#D7F6BFFF' }]}>
+                            {/* Display item name */}
+                            <Text style={styles.itemText}>{item.name}</Text>
+
+                            <View style={{ flexDirection: 'row', gap: 30 }}>
+                                {/* Display item stock and unit */}
+                                <Text style={styles.itemText}>{item.stock} {item.unit}</Text>
+
+                                {/* Edit button */}
+                                <Pressable>
+                                    <Text style={styles.itemText} onPress={() => editItemHandler(item)}>Edit</Text>
+                                </Pressable>
+
+                                {/* Delete button */}
+                                <Pressable>
+                                    <Text style={styles.itemText} onPress={() => deleteItemHandler(item.id)}>Delete</Text>
+                                </Pressable>
+                            </View>
+                        </View>
+                    )}
+                    contentContainerStyle={{ gap: 10 }}
+                ></FlatList>
+            </View>
         </View>
-    )
-}
+    );
+};
 
-export default Create
+export default Create;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        paddingVertical: "4%",
+        gap: 10,
+    },
+    input: {
+        borderWidth: 1.2,
+        borderColor: '#D7F6BFFF',
+        borderRadius: 5,
+        padding: 10,
+    },
+    btn: {
+        // borderWidth: 1.2,
+        // borderColor: '#D7F6BFFF',
+        borderRadius: 5,
+        padding: 10,
+        backgroundColor: '#CABFEEFF',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    // headingContainer: {
+    //     flexDirection: 'row',
+    //     justifyContent: 'space-between',
+    //     paddingHorizontal: 15,
+    //     paddingVertical: 10,
+    // },
+    headingText: {
+        fontSize: 15,
+        fontWeight: '500',
+        marginVertical: 10,
+    },
+    itemContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingHorizontal: 15,
+        paddingVertical: 10,
+        borderBottomWidth: 0.5,
+        borderBottomColor: '#ccc',
+        borderRadius: 7,
+    },
+    itemText: {
+        fontSize: 14
+    }
+})
--- ReactNative/src/components/simpleCRUDProject/screens/HomeScreen.jsx
+++ ReactNative/src/components/simpleCRUDProject/screens/HomeScreen.jsx
@@ -1,27 +1,27 @@
 import { Pressable, StyleSheet, Text, View } from 'react-native'
 import Alltems from './Alltems';
-import LowStock from './LowStock';
 import Create from './Create';
 import { useState } from 'react';
 
-const data = [
-    { id: 1, name: 'Wheat', stock: 5, unit: 'kg' },
-    { id: 2, name: 'Rice', stock: 15, unit: 'kg' },
-    { id: 3, name: 'Sugar', stock: 25, unit: 'kg' },
-    { id: 4, name: 'Salt', stock: 0, unit: 'kg' },
-    { id: 5, name: 'Flour', stock: 50, unit: 'kg' },
-    { id: 6, name: 'Oil', stock: 3, unit: 'L' },
-    { id: 7, name: 'Butter', stock: 1, unit: 'kg' },
-]
-
 const HomeScreen = () => {
 
+    // State to manage the current view (0: All Items, 1: Low Stock, 2: Create)
     const [view, setView] = useState(0);
+
+    // State to manage the list of items
+    const [data, setData] = useState([
+        { id: 1, name: 'Wheat', stock: 5, unit: 'kg' },
+        { id: 2, name: 'Rice', stock: 15, unit: 'kg' },
+        { id: 3, name: 'Sugar', stock: 25, unit: 'kg' },
+        { id: 4, name: 'Flour', stock: 50, unit: 'kg' },
+        { id: 5, name: 'Butter', stock: 1, unit: 'kg' },
+    ]);
 
     return (
         <View style={styles.container}>
             <Text style={styles.title}>Dashboard</Text>
 
+            {/* Buttons to switch between views */}
             <View style={styles.buttonContainer}>
                 <Pressable style={[styles.button, view === 0 ? { backgroundColor: "#72C37AFF" } : null]} onPress={() => setView(0)}>
                     <Text style={[styles.btnText, view === 0 ? { color: "white" } : null]}>All Items</Text>
@@ -36,9 +36,10 @@ const HomeScreen = () => {
                 </Pressable>
             </View>
 
-            {view === 0 && <Alltems data={data} />}
-            {view === 1 && <Alltems data={data.filter((item) => item.stock < 20)} />}
-            {view === 2 && <Create />}
+            {/* Conditional rendering based on the selected view */}
+            {view === 0 && <Alltems data={data} />} {/* Display all items */}
+            {view === 1 && <Alltems data={data.filter((item) => item.stock < 20)} />} {/* Display items with low stock */}
+            {view === 2 && <Create data={data} setData={setData} />} {/* Display create item form */}
         </View>
     )
 }
@@ -56,7 +57,6 @@ const styles = StyleSheet.create({
         fontSize: 24,
         fontWeight: 'bold',
         color: '#333',
-        // marginBottom: 20,
     },
     buttonContainer: {
         flexDirection: 'row',
